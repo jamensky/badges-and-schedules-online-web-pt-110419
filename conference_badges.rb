918660bc8017436c9508f8da2aea2d4d44c2d3de
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-14 17:43:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,8 +17,6 @@ def assign_rooms(room)
 end 
 
 def printer(batch_badge_creator, assign_rooms)
-  t = []
-  t.push(batch_badge_creator.each {|x| puts x})
-  t.push(assign_rooms.each {|y| puts y})
-  return t
+  batch_badge_creator.each {|x| puts x}
+  assign_rooms.each {|y| puts y}
 end 
